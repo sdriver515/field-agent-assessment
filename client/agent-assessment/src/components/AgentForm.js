369febd84501ec6f6f99ed701dfdda618221dd46
sdriver515 <98633039+sdriver515@users.noreply.github.com>
2022-10-17 15:30:27 -0400

    Update client/agent-assessment/src/components/AgentForm.js
    Co-authored-by: Brit Hemming <hello@brittanyhemming.com>

--- client/agent-assessment/src/components/AgentForm.js
+++ client/agent-assessment/src/components/AgentForm.js
@@ -39,7 +39,7 @@ useEffect(() => {
     setAgent(newAgent);
 };
 
-const handleSubmit = (event) => {
+const handleSubmit = event => {
     event.preventDefault();
     if(editAgentId === 0){
         addAgent();
